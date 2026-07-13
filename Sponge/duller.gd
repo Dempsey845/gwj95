174ed81f0707a2e653fa4d3b00ac9c6d5fc48b06
Dempsey845 <dempseynewton1@gmail.com>
2026-07-13 20:40:43 +0100

    Created a WaterLevel to track the players water levels. And tied the duller to it.

--- Sponge/duller.gd
+++ Sponge/duller.gd
@@ -1,14 +1,19 @@
 extends MeshInstance3D
 
+@export var water_level: WaterLevel
+
 var material: ShaderMaterial
 
 func _ready():
 	material = get_active_material(0)
 
+	water_level.water_level_changed.connect(func(new_water_level: float):
+		set_dull_amount(1.0 - (new_water_level / water_level.max_water_level))
+	)
+
 func set_dull_amount(dull_amount: float):
-	if dull_amount > 0:
-		dull_amount /= 2
-		
+	dull_amount = clampf(dull_amount, 0.0, 0.5)
+
 	material.set_shader_parameter(
 		"dull_amount",
 		dull_amount
---
+++ Sponge/player_water_level.gd
@@ -0,0 +1,18 @@
+class_name WaterLevel
+extends Node
+
+signal water_level_changed(new_water_level: float)
+
+var _current_water_level: float = 75.0
+var current_water_level: float:
+    get():
+        return _current_water_level
+    set(value):
+        _current_water_level = value
+        water_level_changed.emit(value)
+
+var max_water_level: float = 100.0
+
+func _ready() -> void:
+    await get_tree().process_frame
+    water_level_changed.emit(current_water_level)
