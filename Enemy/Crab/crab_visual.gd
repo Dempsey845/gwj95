c088edb624edf8605e40ccd638498f0e3a85a47a
Dempsey845 <dempseynewton1@gmail.com>
2026-07-14 11:47:52 +0100

    Imported the crab visuals and created a crab visual script for animations.

---
+++ Enemy/Crab/crab_visual.gd
@@ -0,0 +1,38 @@
+extends Node3D
+
+@export var crab_body: CharacterBody3D
+
+@onready var animation_tree: AnimationTree = $AnimationTree
+
+var is_moving: bool = false
+
+var move_tween: Tween
+
+func _process(_delta: float) -> void:
+	if !crab_body.velocity.is_zero_approx() and !is_moving:
+		is_moving = true
+		blend_move(1.0)
+	else:
+		is_moving = false
+		blend_move(0.0)
+
+func blend_move(target: float):
+	if move_tween:
+		move_tween.stop()
+	
+	move_tween = create_tween()
+
+	move_tween.tween_method(
+		func(value):
+			animation_tree.set(
+				"parameters/Speed/blend_amount",
+				value
+			),
+		animation_tree.get(
+			"parameters/Speed/blend_amount"
+		),
+		target,
+		0.5
+	)
+
+	animation_tree.get("parameters/Speed/blend_amount")
