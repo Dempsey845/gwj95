5a9dccac59e890af1effee11d755a23d0b9bc5f9
Dempsey845 <dempseynewton1@gmail.com>
2026-07-15 16:53:09 +0100

    Imported Terrain3D and started working on the Worlds terrain. Also created a mushroom spawner that works with a MultiMeshInstance3D. Added trees. Imported the Tortoise NPC model.

---
+++ World/multi_mushroom.gd
@@ -0,0 +1,25 @@
+extends MultiMeshInstance3D
+
+@export var mushroom_count := 500
+@export var area_size := Vector2(100, 100)
+
+func _ready():
+	multimesh.instance_count = mushroom_count
+
+	for i in mushroom_count:
+		var pos = Vector3(
+			randf_range(-area_size.x / 2.0, area_size.x / 2.0),
+			0.0,
+			randf_range(-area_size.y / 2.0, area_size.y / 2.0)
+		)
+
+		var shroom_rot = randf_range(0.0, TAU)
+		var shroom_scale= randf_range(0.8, 3.0)
+
+		var shroom_basis = Basis()
+		shroom_basis = shroom_basis.rotated(Vector3.UP, shroom_rot)
+		shroom_basis = shroom_basis.scaled(Vector3.ONE * shroom_scale)
+
+		var shroom_transform = Transform3D(shroom_basis, pos)
+
+		multimesh.set_instance_transform(i, shroom_transform)
