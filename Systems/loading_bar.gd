781d6a6ea9894167ba074ff9058f5a12b708993d
Dempsey845 <dempseynewton1@gmail.com>
2026-07-16 15:02:10 +0100

    Created a loading screen.

---
+++ Systems/loading_bar.gd
@@ -0,0 +1,17 @@
+extends ProgressBar
+
+func _ready() -> void:
+	value = 0.0
+	start_loading()
+
+func start_loading() -> void:
+	var duration = 3
+
+	var tween= create_tween()
+	tween.set_trans(Tween.TRANS_SINE)
+	tween.set_ease(Tween.EASE_OUT)
+	tween.tween_property(self, "value", max_value, duration)
+
+	await tween.finished
+
+	get_parent().get_parent().visible = false
